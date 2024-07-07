d65096ccc4d4039f02d3c08828c6d44b74a1831e
Steve Calla <callasteven@gmail.com>
2024-07-07 17:46:24 -0600

    Adjust server js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,5 @@
 const express = require("express");
 const path = require("path");
-
-const db = require("./config/connection");
-
 const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
@@ -17,13 +14,9 @@ app.use(routes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
 
-  // Uncomment this code once you have built out queries and mutations in the client folder
-  // app.get('*', (req, res) => {
-  // res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-  // });
+  app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
 }
 
-// Comment out this code once you have built out queries and mutations in the client folder
-// db.once('open', () => {
 app.listen(PORT, () => console.log(`Now listening on localhost: ${PORT}`));
-// });
