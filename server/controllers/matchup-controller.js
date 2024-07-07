bafb0a1b8a523b1d3e3479256a6b949f9c0226c7
Steve Calla <callasteven@gmail.com>
2024-07-07 15:08:38 -0600

    Adjust routes

--- server/controllers/matchup-controller.js
+++ server/controllers/matchup-controller.js
@@ -2,43 +2,50 @@ const { Matchup } = require('../models');
 
 module.exports = {
   async createMatchup({ body }, res) {
-    const matchup = await Matchup.create(body);
+    // const matchup = await Matchup.create(body);
 
-    if (!matchup) {
-      return res.status(400).json({ message: 'Unable to create matchup' });
-    }
+    // if (!matchup) {
+    //   return res.status(400).json({ message: 'Unable to create matchup' });
+    // }
 
-    res.status(200).json(matchup);
+    // res.status(200).json(matchup);
+    res.send('hello');
   },
   async createVote(req, res) {
-    const vote = await Matchup.findOneAndUpdate(
-      { _id: req.body.id },
-      { $inc: { [`tech${req.body.techNum}_votes`]: 1 } },
-      { new: true }
-    );
+    // const vote = await Matchup.findOneAndUpdate(
+    //   { _id: req.body.id },
+    //   { $inc: { [`tech${req.body.techNum}_votes`]: 1 } },
+    //   { new: true }
+    // );
 
-    if (!vote) {
-      return res.status(400).json({ message: 'Unable to vote on matchup' });
-    }
+    // if (!vote) {
+    //   return res.status(400).json({ message: 'Unable to vote on matchup' });
+    // }
 
-    res.status(200).json(vote);
+    // res.status(200).json(vote);
+
+    res.send('hello');
   },
   async getAllMatchups(req, res) {
-    const allMatchups = await Matchup.find({});
+    // const allMatchups = await Matchup.find({});
 
-    if (!allMatchups) {
-      return res.status(400).json({ message: 'No matchups found' });
-    }
+    // if (!allMatchups) {
+    //   return res.status(400).json({ message: 'No matchups found' });
+    // }
 
-    res.status(200).json(allMatchups);
+    // res.status(200).json(allMatchups);
+
+    res.send('hello');
   },
   async getMatchup({ params }, res) {
-    const matchup = await Matchup.findOne({ _id: params.id });
+    // const matchup = await Matchup.findOne({ _id: params.id });
 
-    if (!matchup) {
-      return res.status(400).json({ message: 'No matchup found by that id' });
-    }
+    // if (!matchup) {
+    //   return res.status(400).json({ message: 'No matchup found by that id' });
+    // }
 
-    res.status(200).json(matchup);
+    // res.status(200).json(matchup);
+
+    res.send('hello');
   },
 };
