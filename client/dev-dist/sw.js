81740341a390158c3e2ef6ea915b2493e5d3dda0
Steve Calla <callasteven@gmail.com>
2024-07-22 16:49:12 -0600

    Add node modules

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.75af76v7k8g"
+    "revision": "0.ao8qmjo4tug"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
